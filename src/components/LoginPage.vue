fff6983fabeb49baa4c15f0e5bea3818ed4b0cd3
John Jovi Sidabutar <sidabutarjohnjovi@gmail.com>
2024-12-31 19:39:42 +0700

    deployed to app engine

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -54,6 +54,8 @@ export default {
           this.$router.push("/");
         }
       } catch (error) {
+        console.log(error);
+
         this.errorMessage = error.response.data.result;
       }
     },
